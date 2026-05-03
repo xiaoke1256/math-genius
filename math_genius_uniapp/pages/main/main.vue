6095fdc0e5eeafe268fd730e17029aaeb2902c8b
xiaoke_1256 <xiaoke_1256@sina.com>
2026-05-03 14:46:46 +0800

    答对和答错的样式。

--- math_genius_uniapp/pages/main/main.vue
+++ math_genius_uniapp/pages/main/main.vue
@@ -12,19 +12,19 @@
 				</view>
 				<view class="item" >
 					<view class="item-head" >时间</view>
-					<view class="item-value" >4</view>
+					<view class="item-value" >{{countdown}}s</view>
 				</view>
 				<view class="item" >
 					<view class="item-head" >生命</view>
-					<view class="item-value" >4</view>
+					<view class="item-value" >{{hp}}</view>
 				</view>
 				<view class="item" >
 					<view class="item-head" >连击</view>
-					<view class="item-value" >4</view>
+					<view class="item-value" >{{combo}}</view>
 				</view>
 				<view class="item" >
 					<view class="item-head" >得分</view>
-					<view class="item-value" >4</view>
+					<view class="item-value" >{{score}}</view>
 				</view>
 			</view>
 			<view class="progress-area">
@@ -37,10 +37,10 @@
 				<text>3 + 1 = ?</text>
 			</view>
 			<view class="item-area">
-				<u-button class="button" data-item-code="A" @click="onSelectItem">A.  3</u-button>
-				<u-button class="button" data-item-code="B" @click="onSelectItem">B.  2</u-button>
-				<u-button class="button" data-item-code="C" @click="onSelectItem">C.  4</u-button>
-				<u-button class="button" data-item-code="D" @click="onSelectItem">D.  6</u-button>
+				<u-button :class="['button',responseClass('A')]" data-item-code="A" @click="onSelectItem">A.  3</u-button>
+				<u-button :class="['button',responseClass('B')]" data-item-code="B" @click="onSelectItem">B.  2</u-button>
+				<u-button :class="['button',responseClass('C')]" data-item-code="C" @click="onSelectItem">C.  4</u-button>
+				<u-button :class="['button',responseClass('D')]" data-item-code="D" @click="onSelectItem">D.  6</u-button>
 			</view>
 		</view>
 		
@@ -60,8 +60,15 @@
 		},
 		data() {
 			return {
-				progress:0,
-				crrectItemCode:'C'
+				level:1,
+				questionIndex:0,
+				totalQustions:8,
+				countdown:70,
+				hp:3,
+				combo:0,
+				score:0,
+				crrectItemCode:'C',
+				selectedItemCode:''
 			}
 		},
 		onLoad() {
@@ -76,7 +83,29 @@
 				}else{
 					console.log("false");
 				}
+				//显示错误样式半秒钟，然后消失
+				this.selectedItemCode = itemCode;
+				setTimeout(()=>{this.selectedItemCode=''},500);
+				//TODO 更新成绩
+				//半秒后进入下一题，或下一关
 				
+			},
+			responseClass(itemCode) {
+			    if(itemCode==this.selectedItemCode){
+					if(itemCode==this.crrectItemCode){
+						return 'success';
+					}else{
+						return 'danger';
+					}
+				}else{
+					return '';
+				}
+			}
+		
+		},
+		computed: {
+		  	progress(){
+				return Math.floor(this.questionIndex/this.totalQustions*100);
 			}
 		}
 		            
@@ -96,6 +125,12 @@
 		padding-left: 20rpx;
 		padding-right: 20rpx;
 		box-sizing: border-box;
+		.item-head{
+			font-weight: 600;
+		}
+		.item{
+			text-align: center;
+		}
 	}
 	.progress-area{
 		width: 100%;
@@ -126,9 +161,17 @@
 		.button {
 			width: 45%;
 			margin: 0;
+			font-weight: 600;
 		}
-		.danger‌ {
-			
+		
+		.danger {
+			background-color: $u-error-light !important;
+			border: 1rpx solid $u-error !important;
+		}
+		
+		.success {
+			background-color: $u-success-light !important;
+			border: 1rpx solid $u-success !important;
 		}
 	}
 	       
