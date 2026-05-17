1f2f4657c7b5e0e572aedbe10da5943dcb34c1a5
xiaoke_1256 <xiaoke_1256@sina.com>
2026-05-17 15:48:48 +0800

    吐槽页面适应微信

---
+++ math_genius_uniapp/pages/webview/webview.vue
@@ -0,0 +1,16 @@
+<template>
+	<web-view v-if="url" :src="url"></web-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: ''
+			}
+		},
+		onLoad(options) {
+			this.url = decodeURIComponent(options.url || '')
+		}
+	}
+</script>
