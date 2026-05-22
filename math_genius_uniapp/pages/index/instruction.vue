64bfe0c260ed7e748b8e7f6c197db527df8a736a
xiaoke_1256 <xiaoke_1256@sina.com>
2026-05-22 10:39:28 +0800

    题型说明

--- math_genius_uniapp/pages/index/instruction.vue
+++ math_genius_uniapp/pages/index/instruction.vue
@@ -7,7 +7,7 @@
                 <view>
                     <view style="font-weight: 800;"> &bull; 低年级（1-2 年级）：</view>
                     <view> &nbsp; 20以内加减法;</view>
-                    <view> &nbsp; 简单数字比较;</view>
+                    <view> &nbsp; 简单数字比较。</view>
                 </view>
                 <view>
                     <view style="font-weight: 800;"> &bull; 中年级（3-4 年级）：</view>
@@ -15,12 +15,12 @@
                     <view> &nbsp; 表内乘除法;</view>
                     <view> &nbsp; 带余数的乘除法;</view>
                     <view> &nbsp; 整十数乘除法;</view>
-                    <view> &nbsp; 混合运算（不含复杂括号）;</view>
+                    <view> &nbsp; 混合运算（不含复杂括号）。</view>
                 </view>
                 <view >
                     <view style="font-weight: 800;">&bull; 高年级（5-6 年级）：</view>
                     <view> &nbsp; 多位数四则运算;</view>
-                    <view> &nbsp; 简单分数与小数;</view>
+                    <view> &nbsp; 简单分数与小数。</view>
                 </view>
             </view>
         </view>
